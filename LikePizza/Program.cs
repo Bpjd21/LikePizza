5fc945d53c32fa93c92120918476cf7d2a134948
Bpjd21 <bpjd21@yahoo.com>
2019-01-27 23:03:25 -0500

    final Code

--- LikePizza/Program.cs
+++ LikePizza/Program.cs
@@ -6,7 +6,24 @@ namespace LikePizza
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string likePizza;
+
+            Console.WriteLine("Do you like pizza? (yes/no)");
+            likePizza = Console.ReadLine();
+
+            if (likePizza == "yes")
+            {
+                Console.WriteLine("You like Pizza");
+            }
+            if (likePizza == "no")
+            {
+                Console.WriteLine("You don't like Pizza");
+            }
+            else
+            {
+                Console.WriteLine("You entered an invalid response, please answer yes or no");
+            }
+            Console.ReadLine();
         }
     }
 }
