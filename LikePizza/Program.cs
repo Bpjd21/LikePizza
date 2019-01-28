01fd5660f6be0bbe1b4c140a899136c3dd65a1eb
Bpjd21 <bpjd21@yahoo.com>
2019-01-27 22:51:09 -0500

    Practice

---
+++ LikePizza/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LikePizza
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
